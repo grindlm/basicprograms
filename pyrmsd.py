56e9afa7f28be706f7556addefe996ed63d78cb1
grindlm <grindlm@miamioh.edu>
2020-07-30 15:54:12 -0400

    updated pyrmsd.py with colors and domain names

--- pyrmsd.py
+++ pyrmsd.py
@@ -8,14 +8,14 @@ data=np.genfromtxt(sys.argv[1])
 
 if len(sys.argv) > 2:
 	sys.exit('too many args')
-	
+
 xdat = data[:,0]
 ydat = data[:,1]
 ym = max(ydat) + 5
 
 #plt.plot(xdat,ydat)
-plt.xlabel('Residue')
-plt.ylabel('$\delta$q($\AA$)')
+plt.xlabel('Residue', fontsize='large')
+plt.ylabel('$\delta$q($\AA$)',fontsize='large')
 
 if len(xdat)<623:
 	plt.axvline(x=383,color='k',linestyle='--')
@@ -31,12 +31,20 @@ else:
 	mddat = data[227:488]
 	ctddat = data[488:624]
 	plt.axvline(x=227,color='k',linestyle='--')
+	plt.text(217,82,'227',fontsize='small')
 	plt.axvline(x=489,color='k',linestyle='--')
-	plt.plot([1,227],[ym,ym], label='NTD', color='green')
-	plt.plot([227,489],[ym,ym], label='MD', color='orange')
-	plt.plot([489,624],[ym,ym], label='CTD', color='blue')
-	plt.plot(nbddat[:,0],nbddat[:,1],color='green')
-	plt.plot(mddat[:,0],mddat[:,1],color='orange')
+	plt.text(477,82,'489',fontsize='small')
+	#plt.plot([1,227],[ym,ym], label='NTD', color='yellow')
+	#plt.plot([227,489],[ym,ym], label='MD', color='forestgreen')
+	#plt.plot([489,624],[ym,ym], label='CTD', color='blue')
+	ntd=227/2
+	md=(227+489)/2
+	ctd=(489+624)/2
+	plt.text(ntd,(ym-10),'NTD',color='y',fontsize='x-large')
+	plt.text(md,(ym-10),'MD', color='forestgreen',fontsize='x-large')
+	plt.text(ctd,(ym-10),'CTD',color='blue',fontsize='x-large')
+	plt.plot(nbddat[:,0],nbddat[:,1],color='y')
+	plt.plot(mddat[:,0],mddat[:,1],color='forestgreen')
 	plt.plot(ctddat[:,0],ctddat[:,1],color='blue')
 	if len(xdat) > 625:
 		nbddat2 = data[625:(625+226)]
@@ -50,14 +58,5 @@ else:
 		plt.plot(ctddat2[:,0],ctddat2[:,1],color='blue')
 
 
-plt.show()
 
-#	ntd=227/2
-#	md=(227+489)/2
-#	ctd=(489+624)/2
-#	plt.text(ntd,(ym-5),'NTD',color='green')
-#	plt.text(md,(ym-5),'MD', color='orange')
-#	plt.text(ctd,(ym-5),'CTD',color='blue')
-#		plt.text((624+ntd),(ym-5),'NTD',color='green')
-#		plt.text((624+md),(ym-5),'MD', color='orange')
-#		plt.text((624+ctd),(ym-5),'CTD',color='blue')
+plt.show()
