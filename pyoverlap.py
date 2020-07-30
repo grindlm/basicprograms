9aff7c2ad4b03cfd8f5ab1d023ecf443e3f23a20
grindlm <grindlm@miamioh.edu>
2020-07-30 09:19:03 -0400

    modes shown in pyoverlap.py

--- pyoverlap.py
+++ pyoverlap.py
@@ -5,10 +5,12 @@ import numpy as np
 import sys
 import matplotlib
 from operator import itemgetter
+import math
 
 args=sys.argv[1:]
 num_args=len(args)
-each_color = ['black','grey','blue','green','red','purple','orange','magenta','cyan','forestgreen']
+each_color = ['black','blue','red','green','purple','orange','magenta','cyan','forestgreen']
+ymax = 0
 for i in range(num_args):
 	ov=np.genfromtxt(args[i])
 	if ov[0][0] == 6:
@@ -23,6 +25,16 @@ for i in range(num_args):
 	ovs = sorted(ov,key=itemgetter(1),reverse=True)
 	x = 1
 	j = 0
+	x1, x2, x3, x4 = ovs[0][0], ovs[1][0], ovs[2][0], ovs[3][0]
+	y1, y2, y3, y4 = ovs[0][1], ovs[1][1], ovs[2][1], ovs[3][1]
+	if y1>ymax: ymax=y1
+	plt.text(70,(ymax-0.15*i*ymax),r"Mode={}, I$_M$={}".format(int(x1),math.trunc(y1*100)/100.0), color=each_color[i], fontsize='x-large')
+	plt.text(70,(0.95*ymax)-(0.15*i*ymax),r"Mode={}, I$_M$={}".format(int(x2),math.trunc(y2*100)/100.0), color=each_color[i], fontsize='x-large')
+	plt.text(70,(0.9*ymax)-(0.15*i*ymax),r"Mode={}, I$_M$={}".format(int(x3),math.trunc(y3*100)/100.0), color=each_color[i], fontsize='x-large')
+	#plt.text(70,(0.85*ymax)-(0.15*i*ymax),r"Mode={}, I$_M$={}".format(int(x4),math.trunc(y4*100)/100.0), color=each_color[i], fontsize='x-large')
+	#plt.annotate(f"({int(x1)}, {math.trunc(y1*100.0)/100.0})", xy=(x1,y1), xytext=(x1+8,y1+(0.1*ymax)), arrowprops=dict(color=each_color[i], arrowstyle='wedge'))
+	#plt.annotate(f"({int(x2)}, {math.trunc(y2*100.0)/100.0})", xy=(x2,y2), xytext=(x2+8,y2+(0.1*ymax)), arrowprops=dict(color=each_color[i], arrowstyle='wedge'))
+	#plt.annotate(f"({int(x3)}, {math.trunc(y3*100.0)/100.0})", xy=(x3,y3), xytext=(x3+8,y3+(0.1*ymax)), arrowprops=dict(color=each_color[i], arrowstyle='wedge'))
 	while x > 0.25:
 		print(int(ovs[j][0]),'\t',ovs[j][1])
 		j = j+1
@@ -34,10 +46,9 @@ for i in range(num_args):
 		#print(xd,"\t",yd)
 		#plt.text(xd+1.5,yd,'Mode %s, %s' %(xd,yd), color='magenta')
 	print(' ')
-		
+
 
 plt.xlabel('M',fontsize=20)
 plt.ylabel('I$_M$',fontsize=20)
 
 plt.show()
-
