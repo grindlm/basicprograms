7add72ca58f1d67b37ceeddc6c1ccfaaf71a4cb1
grindlm <grindlm@miamioh.edu>
2020-08-12 19:20:04 -0400

    covariance program update

--- corrcov.py
+++ corrcov.py
@@ -17,7 +17,7 @@ dfsorted = df[::-1]
 
 length_of_complex = len(df[0])
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(30,30))
 
 N=256
 vals = np.ones((N,4))
@@ -41,40 +41,85 @@ newcmp = ListedColormap(vals)
 hm = sns.heatmap(dfsorted, cmap=newcmp,center=0,square=True)
 
 if length_of_complex==1248:
+	fntsz = 16
 	dom = [227,489,624,(624+227),(624+489)]
-	plt.yticks([48,148,248,348,448,548,648,748,848,948,1048,1148,1248],[1200,1100,1000,900,800,700,600,500,400,300,200,100,0])
-	plt.xticks([0,100,200,300,400,500,600,700,800,900,1000,1100,1200],np.arange(0,1201,step=100),rotation=45)
+	domainname = [r'NTD$_{(GA)}$', r'MD$_{(GA)}$', r'CTD$_{(GA)}$', r'NTD$_{(GB)}$', r'MD$_{(GB)}$', r'CTD$_{(GB)}$']
+	domaincolor = ['gold', 'forestgreen', 'blue', 'gold', 'forestgreen', 'blue']
+	plt.yticks([48,148,248,348,448,548,648,748,848,948,1048,1148,1248],[1200,1100,1000,900,800,700,600,500,400,300,200,100,0], fontsize=12)
+	plt.xticks([0,100,200,300,400,500,600,700,800,900,1000,1100,1200],np.arange(0,1201,step=100),rotation=45, fontsize=12)
 elif length_of_complex==1853:
+	fntsz = 14
 	dom = [227,489,624,(624+227),(624+489),1248,(1248+390),(1248+510)]
-	plt.xticks(np.arange(0,1853,step=100),np.arange(0,1853,step=100),rotation=45)
-	plt.yticks([53,153,253,353,453,553,653,753,853,953,1053,1153,1253,1353,1453,1553,1653,1753,1853],[1800,1700,1600,1500,1400,1300,1200,1100,1000,900,800,700,600,500,400,300,200,100,0])
+	domainname = [r'NTD$_{(GA)}$', r'MD$_{(GA)}$', r'CTD$_{(GA)}$', r'NTD$_{(GB)}$', r'MD$_{(GB)}$', r'CTD$_{(GB)}$', r'NBD$_{(K)}$', r'SBD$\beta_{(K)}$', r'SBD$\alpha_{(K)}$']
+	domaincolor = ['gold', 'forestgreen', 'blue', 'gold', 'forestgreen', 'blue', 'grey', 'red', 'salmon']
+	plt.xticks(np.arange(0,1853,step=100),np.arange(0,1853,step=100),rotation=45, fontsize=12)
+	plt.yticks([53,153,253,353,453,553,653,753,853,953,1053,1153,1253,1353,1453,1553,1653,1753,1853],[1800,1700,1600,1500,1400,1300,1200,1100,1000,900,800,700,600,500,400,300,200,100,0], fontsize=12)
 elif length_of_complex==2458:
+	fntsz = 9
 	dom = [227,489,624,(624+227),(624+489),1248,(1248+390),(1248+510),1853,(1853+390),(1853+510)]
-	plt.xticks(np.arange(0,2458,step=100),np.arange(0,2458,step=100),rotation=45)
-	plt.yticks([58,158,258,358,458,558,658,758,858,958,1058,1158,1258,1358,1458,1558,1658,1758,1858, 1958,2058,2158,2258,2358,2458],[2400,2300,2200,2100,2000,1900,1800,1700,1600,1500,1400,1300,1200,1100,1000,900,800,700,600,500,400,300,200,100,0])
+	domainname = [r'NTD$_{(GA)}$', r'MD$_{(GA)}$', r'CTD$_{(GA)}$', r'NTD$_{(GB)}$', r'MD$_{(GB)}$', r'CTD$_{(GB)}$', r'NBD$_{(KA)}$', r'SBD$\beta_{(KA)}$', r'SBD$\alpha_{(KA)}$', r'NBD$_{(KB)}$', r'SBD$\beta_{(KB)}$', r'SBD$\alpha_{(KB)}$']
+	domaincolor = ['gold', 'forestgreen', 'blue', 'gold', 'forestgreen', 'blue', 'grey', 'red', 'salmon', 'grey', 'red', 'salmon']
+	plt.xticks(np.arange(0,2458,step=100),np.arange(0,2458,step=100),rotation=45, fontsize=12)
+	plt.yticks([58,158,258,358,458,558,658,758,858,958,1058,1158,1258,1358,1458,1558,1658,1758,1858, 1958,2058,2158,2258,2358,2458],[2400,2300,2200,2100,2000,1900,1800,1700,1600,1500,1400,1300,1200,1100,1000,900,800,700,600,500,400,300,200,100,0], fontsize=12)
 elif length_of_complex==605:
+	fntsz = 20
 	dom = [390,510]
-	plt.xticks(np.arange(0,601,step=100),np.arange(0,601,step=100),rotation=45)
-	plt.yticks([5,105,205,305,405,505,605],[600,500,400,300,200,100,0])
+	domainname = [r'NBD$_{(K)}$', r'SBD$\beta_{(K)}$', r'SBD$\alpha_{(K)}$']
+	domaincolor = ['grey', 'red', 'salmon']
+	plt.xticks(np.arange(0,601,step=100),np.arange(0,601,step=100),rotation=45, fontsize=12)
+	plt.yticks([5,105,205,305,405,505,605],[600,500,400,300,200,100,0], fontsize=12)
 elif length_of_complex==604:
+	fntsz = 20
 	dom = [390,510]
-	plt.xticks(np.arange(0,601,step=100),np.arange(0,601,step=100),rotation=45)
-	plt.yticks([4,104,204,304,404,504,604],[600,500,400,300,200,100,0])
+	domainname = [r'NBD$_{(K)}$', r'SBD$\beta_{(K)}$', r'SBD$\alpha_{(K)}$']
+	domaincolor = ['grey', 'red', 'salmon']
+	plt.xticks(np.arange(0,601,step=100),np.arange(0,601,step=100),rotation=45, fontsize=12)
+	plt.yticks([4,104,204,304,404,504,604],[600,500,400,300,200,100,0], fontsize=12)
 elif length_of_complex==624:
+	fntsz = 20
 	dom = [227,489]
+	domainname = ['NTD', 'MD', 'CTD', 'NTD', 'MD', 'CTD']
+	domaincolor = ['gold', 'forestgreen', 'blue']
 elif length_of_complex==1357:
+	fntsz = 20
 	dom = [390,510,605,981]
-	plt.xticks(np.arange(0,1357,step=100),np.arange(0,1357,step=100),rotation=45)
-	plt.yticks([57,157,257,357,457,557,657,757,857,957,1057,1157,1257,1357],[1300,1200,1100,1000,900,800,700,600,500,400,300,200,100,0])
+	plt.xticks(np.arange(0,1357,step=100),np.arange(0,1357,step=100),rotation=45, fontsize=12)
+	plt.yticks([57,157,257,357,457,557,657,757,857,957,1057,1157,1257,1357],[1300,1200,1100,1000,900,800,700,600,500,400,300,200,100,0], fontsize=12)
 elif length_of_complex==923:
+	fntsz = 20
 	dom = []
 else:
+	fntsz = 20
 	dom = []
 	print(length_of_complex, "is not an expected number of residues")
 
 dominv=[]
 if len(dom)>0:
+	ax.text(length_of_complex,-5,f"{length_of_complex - dom[-3]}",ha='center', fontsize=8)
 	for i in range(len(dom)):
+		if i == 0:
+			ax.text(dom[i]/2,-80, "{}".format(domainname[i]),ha='center',fontsize=fntsz, color=f"{domaincolor[i]}")
+			print("first domain --> {} {} {}".format(dom[i], domainname[i], domaincolor[i]))
+		elif dom[i] == dom[-1]:
+			ax.text((dom[i]+dom[i-1])/2, -80, "{}".format(domainname[i]),ha='center',fontsize=fntsz, color=f"{domaincolor[i]}")
+			ax.text((dom[i]+length_of_complex)/2, -80, "{}".format(domainname[i+1]), fontsize=fntsz, color=f"{domaincolor[i+1]}")
+			print("This is the {} domain --> {} {} {}".format(i, dom[i], domainname[i], domaincolor[i]))
+			print("{} domain --> {} {} {}".format(i+1, dom[i], domainname[i+1], domaincolor[i+1]))
+		else:
+			if domainname[i]==r'SBD$\alpha_{(KA)}$':
+				hac = 'left'
+			else:
+				hac = 'center'
+			ax.text((dom[i]+dom[i-1])/2, -80, "{}".format(domainname[i]),ha=hac,fontsize=fntsz, color=f"{domaincolor[i]}")
+			print("{} domain --> {} {} {}".format(i, dom[i], domainname[i], domaincolor[i]))
+		if dom[i] < 624:
+			ax.text(dom[i],(-5),f"{dom[i]}",fontsize=8, ha='center')
+		elif 624 <= dom[i] < 1248:
+			ax.text(dom[i],(-5),f"{dom[i] - 624}",fontsize=8, ha='center')
+		elif 1248 <= dom[i] < 1853:
+			ax.text(dom[i],(-5),f"{dom[i] - 1248}",fontsize=8, ha='center')
+		elif 1853 <= dom[i] < 2458:
+			ax.text(dom[i],(-5),f"{dom[i] - 1853}",fontsize=8, ha='center')
 		dominv.append(abs(dom[i]-length_of_complex))
 		dominv.sort()
 	ax.vlines(dom, 0, length_of_complex, linewidth=3, color='k')
--- pyrmsd.py
+++ pyrmsd.py
@@ -9,54 +9,62 @@ data=np.genfromtxt(sys.argv[1])
 if len(sys.argv) > 2:
 	sys.exit('too many args')
 
+
 xdat = data[:,0]
 ydat = data[:,1]
-ym = max(ydat) + 5
+def ymax(domain):
+	return (max(domain[:,1]) + 5)
 
+fig, ax = plt.subplots(figsize=(10,6))
 #plt.plot(xdat,ydat)
 plt.xlabel('Residue', fontsize='large')
 plt.ylabel('$\delta$q($\AA$)',fontsize='large')
 
 if len(xdat)<623:
-	plt.axvline(x=383,color='k',linestyle='--')
-	plt.axvline(x=510, color='k',linestyle='--')
+	ax.axvline(x=383,color='k',linestyle='--')
+	ax.axvline(x=510, color='k',linestyle='--')
 	nbddat=data[:383]
-	sbdbetdat=data[384:509]
+	linker=data[383:391]
+	sbdbetdat=data[390:509]
 	sbdalpdat=data[510:]
-	plt.plot(nbddat[:,0],nbddat[:,1],color='grey')
-	plt.plot(sbdbetdat[:,0],sbdbetdat[:,1],color='purple')
-	plt.plot(sbdalpdat[:,0],sbdalpdat[:,1],color='teal')
+	ax.text(373,ymax(sbdalpdat)+3,'383',fontsize='small')
+	ax.text(500,ymax(sbdalpdat)+3,'510',fontsize='small')
+	ax.plot(nbddat[:,0],nbddat[:,1],color='grey')
+	ax.text(((383/2)),(ymax(nbddat)),'NBD',color='grey',fontsize='x-large')
+	ax.plot(linker[:,0],linker[:,1],color='cyan')
+	ax.plot(sbdbetdat[:,0],sbdbetdat[:,1],color='red')
+	ax.text(((383+383+510)/3),(ymax(sbdbetdat)), r'SBD$\beta$',color='red',fontsize='x-large')
+	ax.plot(sbdalpdat[:,0],sbdalpdat[:,1],color='salmon')
+	ax.text(((510+605+510)/3),(ymax(sbdalpdat)-10), r'SBD$\alpha$',color='salmon',fontsize='x-large')
 else:
 	nbddat = data[:226]
 	mddat = data[227:488]
 	ctddat = data[488:624]
-	plt.axvline(x=227,color='k',linestyle='--')
-	plt.text(217,82,'227',fontsize='small')
-	plt.axvline(x=489,color='k',linestyle='--')
-	plt.text(477,82,'489',fontsize='small')
-	#plt.plot([1,227],[ym,ym], label='NTD', color='yellow')
-	#plt.plot([227,489],[ym,ym], label='MD', color='forestgreen')
-	#plt.plot([489,624],[ym,ym], label='CTD', color='blue')
+	ax.axvline(x=227,color='k',linestyle='--')
+	ax.text(217,82,'227',fontsize='small')
+	ax.axvline(x=489,color='k',linestyle='--')
+	ax.text(477,82,'489',fontsize='small')
+	#ax.plot([1,227],[ym,ym], label='NTD', color='yellow')
+	#ax.plot([227,489],[ym,ym], label='MD', color='forestgreen')
+	#ax.plot([489,624],[ym,ym], label='CTD', color='blue')
 	ntd=227/2
 	md=(227+489)/2
 	ctd=(489+624)/2
-	plt.text(ntd,(ym-10),'NTD',color='y',fontsize='x-large')
-	plt.text(md,(ym-10),'MD', color='forestgreen',fontsize='x-large')
-	plt.text(ctd,(ym-10),'CTD',color='blue',fontsize='x-large')
-	plt.plot(nbddat[:,0],nbddat[:,1],color='y')
-	plt.plot(mddat[:,0],mddat[:,1],color='forestgreen')
-	plt.plot(ctddat[:,0],ctddat[:,1],color='blue')
+	ax.text(ntd,(ymax(nbddat)-10),'NTD',color='gold',fontsize='x-large')
+	ax.text(md,(ymax(mddat)-12),'MD', color='forestgreen',fontsize='x-large')
+	ax.text(ctd,(ymax(ctddat)-10),'CTD',color='blue',fontsize='x-large')
+	ax.plot(nbddat[:,0],nbddat[:,1],color='gold')
+	ax.plot(mddat[:,0],mddat[:,1],color='forestgreen')
+	ax.plot(ctddat[:,0],ctddat[:,1],color='blue')
 	if len(xdat) > 625:
 		nbddat2 = data[625:(625+226)]
 		mddat2 = data[(625+227):(625+488)]
 		ctddat2 = data[(625+488):1248]
-		plt.axvline(x=624,color='k',linestyle='-')
-		plt.axvline(x=(624+227),color='k',linestyle='--')
-		plt.axvline(x=(624+489),color='k',linestyle='--')
-		plt.plot(nbddat2[:,0],nbddat2[:,1], label='NTD', color='green')
-		plt.plot(mddat2[:,0],mddat2[:,1],color='orange')
-		plt.plot(ctddat2[:,0],ctddat2[:,1],color='blue')
-
-
+		ax.axvline(x=624,color='k',linestyle='-')
+		ax.axvline(x=(624+227),color='k',linestyle='--')
+		ax.axvline(x=(624+489),color='k',linestyle='--')
+		ax.plot(nbddat2[:,0],nbddat2[:,1], label='NTD', color='gold')
+		ax.plot(mddat2[:,0],mddat2[:,1],color='forestgreen')
+		ax.plot(ctddat2[:,0],ctddat2[:,1],color='blue')
 
 plt.show()
